66a9e35f40e528c3e2ef2f5c30800d1fbdb97c03
JahanviChamria <jahanvi.chamria@gmail.com>
2025-09-04 09:28:42 -0400

    Question 2 (calculations)

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,22 @@
 print("Hello World")
+6^6
+234+8
+15-23
+a<-6234
+a*5
+a/3
+b <- c(2395,82,2947)
+b/2
+
+# Question 2
+# creating vectors with x, y, z coordinates and time
+position1 <- c(0, 3, 5, 2) 
+position2 <- c(-3, 8, 7, 0) 
+position3 <- c(9, -4, 2, 1) 
+position4 <- c(1, 1, 6, 4) 
+
+position1+position4
+position2*position3
+position1+position2+position3+position4
+position4-position2
+position3/2
